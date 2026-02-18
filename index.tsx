e6c53b331a5566938958ae936b89b01dde756ee1
ManuelHondermann <manuelhondermannsueldo@gmail.com>
2026-02-18 12:05:50 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App'; // Eliminada la extensión .tsx para cumplir con las reglas de TS
 
 const container = document.getElementById('root');
 
