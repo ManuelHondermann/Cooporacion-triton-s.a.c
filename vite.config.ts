67e4c85f87a9f60a064420d4b9f20a3ab9d43e1a
ManuelHondermann <manuelhondermannsueldo@gmail.com>
2026-02-18 11:51:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // El base debe coincidir exactamente con el nombre de tu repo en GitHub para subcarpetas
+  base: '/Cooporacion-triton-s.a.c/',
   build: {
     outDir: 'dist'
   }
